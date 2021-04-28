70ba66328642b8aee233ee585b5ece792acf9c7b
NetLSS <38928282+NetLSS@users.noreply.github.com>
2021-04-28 09:54:01 +0900

    Feat: 버튼 클릭 리스너 추가

--- app/src/main/java/lilcode/aop/part2/chapter01/MainActivity.kt
+++ app/src/main/java/lilcode/aop/part2/chapter01/MainActivity.kt
@@ -1,9 +1,12 @@
 package lilcode.aop.part2.chapter01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,26 @@ class MainActivity : AppCompatActivity() {
         val weightEditText = findViewById<EditText>(R.id.weightEditText) //<>에 반환 값이 뭔지 알려줌
 
         val resultButton = findViewById<Button>(R.id.resultButton)
+
+        resultButton.setOnClickListener {
+            // 로그 남기기
+            Log.d("MainActivity", "ResultButton 이 클릭되었습니다.")
+
+            // 빈 값이 들어오는 경우 예외처리
+            if (heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+
+                return@setOnClickListener // setOnClickListener 라는 함수의 return 임을 명시
+            }
+
+            val height: Int = heightEditText.text.toString().toInt()
+            val weight: Int = weightEditText.text.toString().toInt()
+
+            Log.d("MainActivity", "height: $height, weight: $weight")
+
+            // MainActivity 에서 ResultActivity 로 넘어가기
+            val intent = Intent(this, ResultActivity::class.java /*클래스 이름*/)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/lilcode/aop/part2/chapter01/ResultActivity.kt
@@ -0,0 +1,13 @@
+package lilcode.aop.part2.chapter01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity() {
+
+    // Activity 실행 되었을 때 동작
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
